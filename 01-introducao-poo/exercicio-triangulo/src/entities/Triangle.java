1779aaf3fbf3f20c5d86318e84b904bb8fd13bd4
joaopedro-devbr <joaopedroandrade712@gmail.com>
2026-09-13 00:51:32 -0300

    cria entidade Triangle com atributo e metodo de area

---
+++ 01-introducao-poo/exercicio-triangulo/src/entities/Triangle.java
@@ -0,0 +1,11 @@
+// Exercício: Problema Triângulo - Introdução à POO
+// Objetivo: Criar a classe de entidade Triangle apenas com os atributos dos três lados.
+
+package entities;
+
+public class Triangle {
+    
+    public double a;
+    public double b;
+    public double c;
+}
