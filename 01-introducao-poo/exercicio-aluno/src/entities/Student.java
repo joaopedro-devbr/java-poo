3720ffb73b6a40f878af2945dbd0e2e2ca031c9b
joaopedro-devbr <joaopedroandrade712@gmail.com>
2026-09-15 20:41:01 -0300

    cria entidade Student com calculo de nota final e pontos restantes
    This class represents a student with attributes for name and grades, and methods to calculate the final grade and missing points for approval.

---
+++ 01-introducao-poo/exercicio-aluno/src/entities/Student.java
@@ -0,0 +1,25 @@
+// Exercício: Aluno - Introdução à POO
+// Objetivo: Representar o aluno, calcular a nota final e verificar a pontuação restante para aprovação.
+
+package entities;
+
+public class Student {
+
+    public String name;
+    public double grade1;
+    public double grade2;
+    public double grade3;
+
+    public double finalGrade() {
+        return grade1 + grade2 + grade3;
+    }
+
+    public double missingPoints() {
+        if (finalGrade() < 60.0) {
+             return 60.0 - finalGrade();
+        }
+        else {
+            return 0.0;
+        }
+    }
+}
