2df8734ce6401eb88250eccae7a780810a9f4ab3
joaopedro-devbr <joaopedroandrade712@gmail.com>
2026-09-23 22:24:00 -0300

    cria entidade Product para manipulacao em vetor

---
+++ 03-comportamento-de-memoria-arrays-listas/exercicio-vetor-produtos/src/entities/Product.java
@@ -0,0 +1,31 @@
+// Exercício: Vetores de Objetos - Preço Médio
+// Objetivo: Entidade de domínio para representar um produto com nome e preço.
+
+package entities;
+
+public class Product {
+
+    private String name;
+    private  double price;
+    
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
